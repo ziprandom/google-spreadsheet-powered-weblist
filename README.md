5f8c49e9dec01e129340474e10b1a4e5e990372d
Zip Random <zip>
2015-12-08 22:30:00 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# google-spreadsheet-powered-weblist
+
+A Single Page App to list rows of a Google Spreadsheet as Items in a List of Organizations.
---
+++ src/app/main/main.controller.ts
@@ -0,0 +1,34 @@
+import { PostRepo, IPost, IPostDictionary } from '../components/postRepo/postRepo.service';
+
+export class MainController {
+  public classAnimation: string;
+  public posts: IPost[];
+  public tags: IPostDictionary;
+  public activeCategory: string = '';
+  public searchActive: bool = false;
+  public searchTerm: string = '';
+  /* @ngInject */
+  constructor ($timeout: angular.ITimeoutService, public postRepo: PostRepo) {
+    var that = this;
+    postRepo.loadUrl("<enter-the-url-of-you-google-spreadsheet-here>").then((data: any[]) => {
+      that.posts = data[0];
+      that.tags = data[1];
+    });
+  }
+
+  public getActiveSelectionPosts(): IPost[] {
+    if (this.activeCategory === '' || this.searchActive ) {
+      return this.posts;
+    } else {
+      return this.tags[this.activeCategory];
+    }
+  }
+
+  public toggleActiveCategory(tag: string) {
+    if (this.activeCategory === tag) {
+      this.activeCategory = '';
+    } else {
+      this.activeCategory = tag;
+    }
+  }
+}
